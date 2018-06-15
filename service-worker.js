446e4b090552644411b4f268a8e4f6b445bc3e08
dangwei <dangwei@yonyou.com>
2018-06-15 22:23:11 +0800

    lavasApp

---
+++ service-worker.js
@@ -0,0 +1,74 @@
+importScripts('/static/js/workbox-sw.prod.v2.1.3.js');/**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author dangwei(dangwei@yonyou.com)
+ */
+
+/* globals WorkboxSW */
+
+const workboxSW = new WorkboxSW({
+    cacheId: 'lavas-cache',
+    ignoreUrlParametersMatching: [/^utm_/],
+    skipWaiting: true,
+    clientsClaim: true
+});
+
+// Define precache injection point.
+workboxSW.precache([
+  {
+    "url": "/index.html",
+    "revision": "42dddf33a6faf9f4be1f840ec948cf7a"
+  },
+  {
+    "url": "/static/css/index.034da158.css"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "url": "/static/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "url": "/static/js/index.eaa35fa9.js"
+  },
+  {
+    "url": "/static/js/manifest.7253881e.js"
+  },
+  {
+    "url": "/static/js/vendor.320806ce.js"
+  },
+  {
+    "url": "/static/js/vue.c5db6194.js"
+  },
+  {
+    "url": "/static/js/workbox-sw.prod.v2.1.3.js",
+    "revision": "a9890beda9e5f17e4c68f42324217941"
+  }
+]);
+workboxSW.router.registerNavigationRoute('/index.html');
+
+
+/**
+ * example runningCache with api
+ */
+// workboxSW.router.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+//     workboxSW.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workboxSW.router.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workboxSW.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         cacheExpiration: {
+//             maxEntries: 100,
+//             maxAgeSeconds: 7 * 24 * 60 * 60
+//         },
+//         cacheableResponse: {
+//             statuses: [0, 200]
+//         }
+//     })
+// );
